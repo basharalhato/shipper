64e16fa86b1280281643717962f27c6b1dc6cd64
Bashar Alhato <basharhato2001@gmail.com>
2023-06-23 11:36:50 +0300

    Edit file

--- lib/features/map/presentation/providers/target_location_providers/target_location_marker_provider.dart
+++ lib/features/map/presentation/providers/target_location_providers/target_location_marker_provider.dart
@@ -1,11 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shipper/features/map/presentation/helpers/map_style_helper.dart';
 import 'package:shipper/features/map/presentation/providers/place_details_provider.dart';
 import 'package:shipper/features/map/presentation/providers/target_location_providers/target_location_camera_position_provider.dart';
 import 'package:shipper/features/map/presentation/providers/target_location_providers/target_location_geo_point_provider.dart';
-import 'package:fpdart/fpdart.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final targetLocationMarkerProvider = StateProvider.autoDispose<Marker>((ref) {
   final cameraTarget = ref.watch(
@@ -26,5 +26,6 @@ final targetLocationMarkerProvider = StateProvider.autoDispose<Marker>((ref) {
       ref.watch(targetLocationGeoPointProvider.notifier).state = position;
     },
   );
+
   return targetMarker;
 });
